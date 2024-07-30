164a13e973030234a57a16d059b16785fc981f72
Lesleis Nagy <lesleisnagy@gmail.com>
2024-07-30 23:42:11 +0100

    Dev update.

--- toy/qt-vtk/ex002/MainWindow.cpp
+++ toy/qt-vtk/ex002/MainWindow.cpp
@@ -6,25 +6,14 @@
 
 #include <QVTKOpenGLNativeWidget.h>
 
-#include <vtkPolyDataMapper.h>
-#include <vtkCubeSource.h>
-#include "vtkCylinderSource.h"
-#include "vtkPlaneSource.h"
-#include "vtkProperty.h"
-#include "vtkCamera.h"
-
 #include "MainWindow.hpp"
 
-MainWindow::MainWindow() :
-_bg_red(0.0), _bg_green(0.0), _bg_blue(0.0),
-_actor_red(1.0), _actor_green(1.0), _actor_blue(1.0) {
-
-  using PtrCylinderSource = vtkSmartPointer<vtkCylinderSource>;
-  using PtrPlaneSource = vtkSmartPointer<vtkPlaneSource>;
-  using PtrPolyDataMapper = vtkSmartPointer<vtkPolyDataMapper>;
-  using PtrActor = vtkSmartPointer<vtkActor>;
-
+MainWindow::MainWindow() : _bg_red(0.0), _bg_green(0.0), _bg_blue(0.0),
+                           _actor_red(1.0), _actor_green(1.0),
+                           _actor_blue(1.0) {
   this->setupUi(this);
+
+  display_camera_mode();
 
   // Create and assign a new vtkRenderer.
   _renderer = vtkSmartPointer<vtkRenderer>::New();
@@ -37,16 +26,16 @@ _actor_red(1.0), _actor_green(1.0), _actor_blue(1.0) {
   cylinder_source->SetResolution(300);
   cylinder_source->Update();
 
-  PtrPolyDataMapper cylinder_poly_data_mapper = PtrPolyDataMapper::New();
-  cylinder_poly_data_mapper->SetInputConnection(cylinder_source->GetOutputPort());
+  _cylinder_poly_data_mapper = PtrPolyDataMapper::New();
+  _cylinder_poly_data_mapper->SetInputConnection(
+    cylinder_source->GetOutputPort());
 
   _cylinder_actor = PtrActor::New();
-  _cylinder_actor->SetMapper(cylinder_poly_data_mapper);
-  _cylinder_actor->GetProperty()->SetColor(_actor_red, _actor_green, _actor_blue);
+  _cylinder_actor->SetMapper(_cylinder_poly_data_mapper);
+  _cylinder_actor->GetProperty()->SetColor(_actor_red, _actor_green,
+                                           _actor_blue);
   _cylinder_actor->RotateX(30.0);
   _cylinder_actor->RotateY(-45.0);
-
-  _renderer->AddActor(_cylinder_actor);
 
   // Set the plane _plane_actor.
   PtrPlaneSource plane_source = PtrPlaneSource::New();
@@ -55,20 +44,69 @@ _actor_red(1.0), _actor_green(1.0), _actor_blue(1.0) {
   //plane_source->SetPoint1( 0, 1, 0 );
   //plane_source->SetPoint2( 1, 0, 0 );
   plane_source->SetCenter(1, 1, 1);
-  plane_source->SetNormal(1, 0, 0);
+  plane_source->SetNormal(1, 1, 0);
   plane_source->Update();
 
-  PtrPolyDataMapper plane_poly_data_mapper = PtrPolyDataMapper::New();
-  plane_poly_data_mapper->SetInputConnection(plane_source->GetOutputPort());
+  _plane_poly_data_mapper = PtrPolyDataMapper::New();
+  _plane_poly_data_mapper->SetInputConnection(plane_source->GetOutputPort());
 
   _plane_actor = PtrActor::New();
-  _plane_actor->SetMapper(plane_poly_data_mapper);
+  _plane_actor->SetMapper(_plane_poly_data_mapper);
   _plane_actor->GetProperty()->SetColor(_actor_red, _actor_green, _actor_blue);
+  _plane_actor->GetProperty()->LightingOff();
 
+  _object_interaction_style = PtrMouseInteractorStyle5::New();
+  _object_interaction_style->SetDefaultRenderer(_renderer);
+  _object_interaction_style->cylinder = _cylinder_actor;
+  _object_interaction_style->plane = _plane_actor;
+
+  _world_interaction_style = PtrTrackballCameraStyle::New();
+  _world_interaction_style->SetDefaultRenderer(_renderer);
+
+  _vtk_widget->renderWindow()->GetInteractor()->SetInteractorStyle(
+    _world_interaction_style);
+
+  _renderer->AddActor(_cylinder_actor);
   _renderer->AddActor(_plane_actor);
 
-  // Finalize the _renderer.
-  _renderer->ResetCamera();
-  _renderer->GetActiveCamera()->Zoom(1.0);
 
+  _renderer->ResetCamera();
+  _renderer->GetActiveCamera()->SetPosition(7 * cos(45.0 * M_PI / 180.0), 1,
+                                            7 * sin(45.0 * M_PI / 180.0));
+  _renderer->GetActiveCamera()->SetFocalPoint(0, 0, 0);
+
+  _vtk_widget->update();
+  _vtk_widget->renderWindow()->Render();
+
+
+  // Hook up signals and slots.
+
+  connect(_btn_toggle, SIGNAL(clicked(bool)),
+          this, SLOT(slot_btn_toggle_clicked()));
+}
+
+void
+MainWindow::slot_btn_toggle_clicked() {
+  if (_camera_mode == WORLD_MODE) {
+    _camera_mode = OBJECT_MODE;
+    _vtk_widget->renderWindow()->GetInteractor()->SetInteractorStyle(
+      _object_interaction_style);
+  } else {
+    _camera_mode = WORLD_MODE;
+    _vtk_widget->renderWindow()->GetInteractor()->SetInteractorStyle(
+      _world_interaction_style);
+  }
+  display_camera_mode();
+}
+
+void
+MainWindow::display_camera_mode() {
+  switch (_camera_mode) {
+    case OBJECT_MODE:
+      _status_bar->showMessage("Object mode");
+      break;
+    case WORLD_MODE:
+      _status_bar->showMessage("World mode");
+      break;
+  }
 }
--- toy/qt-vtk/ex002/MainWindow.hpp
+++ toy/qt-vtk/ex002/MainWindow.hpp
@@ -7,35 +7,109 @@
 
 #include <QMainWindow>
 
-#include <vtkSmartPointer.h>
-#include <vtkRenderWindow.h>
+#include "vtkCamera.h"
+#include "vtkCylinderSource.h"
+#include "vtkPlaneSource.h"
+#include "vtkProperty.h"
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkCubeSource.h>
+#include <vtkInteractorStyleTrackballActor.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
 #include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkSmartPointer.h>
+#include <vtkSphereSource.h>
 
 #include "ui_MainWindow.h"
 
+namespace {
+    // Handle mouse events.
+    class MouseInteractorStyle5 : public vtkInteractorStyleTrackballActor
+    {
+    public:
+        static MouseInteractorStyle5* New();
+        vtkTypeMacro(MouseInteractorStyle5, vtkInteractorStyleTrackballActor);
+
+        void OnLeftButtonDown() override {
+            vtkInteractorStyleTrackballActor::OnLeftButtonDown();
+
+            if (this->InteractionProp == this->cylinder) {
+                std::cout << "Picked cylinder." << std::endl;
+            }
+            else if (this->InteractionProp == this->plane) {
+                std::cout << "Picked plane." << std::endl;
+            }
+        }
+
+        vtkActor* cylinder;
+        vtkActor* plane;
+    };
+    vtkStandardNewMacro(MouseInteractorStyle5);
+}
+
 class MainWindow : public QMainWindow, private Ui::MainWindow {
-  Q_OBJECT
+    Q_OBJECT
 
- public:
+public:
 
-  MainWindow();
-  ~MainWindow() override = default;
+    using PtrCylinderSource = vtkSmartPointer<vtkCylinderSource>;
+    using PtrPlaneSource = vtkSmartPointer<vtkPlaneSource>;
+    using PtrPolyDataMapper = vtkSmartPointer<vtkPolyDataMapper>;
+    using PtrActor = vtkSmartPointer<vtkActor>;
+    using PtrRenderer = vtkSmartPointer<vtkRenderer>;
+    using PtrRenderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>;
+    using PtrMouseInteractorStyle5 = vtkSmartPointer<MouseInteractorStyle5>;
+    using PtrTrackballCameraStyle = vtkSmartPointer<vtkInteractorStyleTrackballCamera>;
 
- private:
+    MainWindow();
 
-  double _bg_red;
-  double _bg_green;
-  double _bg_blue;
+    ~MainWindow() override = default;
 
-  double _actor_red;
-  double _actor_green;
-  double _actor_blue;
+public slots:
 
-  vtkSmartPointer<vtkRenderer> _renderer;
+    void slot_btn_toggle_clicked();
 
-  vtkSmartPointer<vtkActor> _cylinder_actor;
-  vtkSmartPointer<vtkActor> _plane_actor;
+private:
 
+    enum CameraMode {
+        OBJECT_MODE,
+        WORLD_MODE
+    };
+
+    double _bg_red;
+    double _bg_green;
+    double _bg_blue;
+
+    double _actor_red;
+    double _actor_green;
+    double _actor_blue;
+
+    CameraMode _camera_mode = WORLD_MODE;
+
+    PtrCylinderSource _cylinder_source;
+    PtrPolyDataMapper _cylinder_poly_data_mapper;
+    PtrPlaneSource _plane_source;
+    PtrPolyDataMapper _plane_poly_data_mapper;
+
+    PtrRenderer _renderer;
+
+    PtrActor _cylinder_actor;
+    PtrActor _plane_actor;
+
+    PtrRenderWindowInteractor _render_window_interactor;
+
+    PtrMouseInteractorStyle5 _object_interaction_style;
+    PtrTrackballCameraStyle _world_interaction_style;
+
+    void
+    display_camera_mode();
 };
+
 
 #endif // VCOMPARE_TOY_QT_VTK_MAINWINDOWS_HPP_
